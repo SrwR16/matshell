d809670c1af88d370d207c962b4726ae03919575
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-04-10 23:36:59 +0200

    comments and formatting

--- utils/hyprland.ts
+++ utils/hyprland.ts
@@ -1,19 +1,17 @@
 import { App, Gdk } from "astal/gtk4";
 import Hyprland from "gi://AstalHyprland";
 
-/* Get the focused monitor from Hyprland and convert to a GDK monitor
-THIS MAY NOT WORK IF YOU HAVE MONITORS OF THE SAME MODEL
+/* Match Hyprland monitor to GDK monitor
+THIS MAY NOT WORK AS INTENDED IF YOU HAVE MONITORS OF THE SAME MODEL
 I did not find a more elegant solution to this. 
 On my setup GDK coordinates and hyprland coordinates are flipped,
 so I cant match by coordinates. */
 
-export function hyprToGdk(
-  monitor: Hyprland.Monitor,
-): Gdk.Monitor {
+export function hyprToGdk(monitor: Hyprland.Monitor): Gdk.Monitor {
   const monitors = App.get_monitors();
   for (let gdkmonitor of monitors) {
     if (monitor.get_name() === gdkmonitor.get_connector()) return gdkmonitor;
   }
-  // Fallback default monitor
+  // Default monitor
   return monitors[0];
 }
