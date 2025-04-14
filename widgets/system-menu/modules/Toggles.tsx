b037b92bcfb5637cfba6bb433bb83b057b6a1ed8
Neurarian <110474238+Neurarian@users.noreply.github.com>
2025-04-14 20:24:27 +0200

    feat: add wifi controls to system-menu

--- widgets/system-menu/modules/Toggles.tsx
+++ widgets/system-menu/modules/Toggles.tsx
@@ -1,43 +1,14 @@
 import { execAsync, bind } from "astal";
 import Bluetooth from "gi://AstalBluetooth";
-import Network from "gi://AstalNetwork";
-import {
-  getBluetoothIcon,
-  getBluetoothText,
-} from "utils/bluetooth.ts";
+import { WiFiBox } from "./WifiBox.tsx";
+import { getBluetoothIcon, getBluetoothText } from "utils/bluetooth.ts";
 
 export const Toggles = () => {
-  const network = Network.get_default();
   const bluetooth = Bluetooth.get_default();
 
   return (
     <box cssClasses={["toggles"]} vertical>
-      {/* Network Toggle */}
-      <box cssClasses={["toggle"]}>
-        <button
-          onClicked={() => {
-            if (network.wifi.enabled) {
-              network.wifi.set_enabled(false);
-            } else network.wifi.set_enabled(true);
-          }}
-          cssClasses={bind(network, "connectivity").as((c) =>
-            c === Network.Connectivity.FULL ? ["button"] : ["button-disabled"],
-          )}
-        >
-          <image iconName={bind(network.wifi, "icon_name")} />
-        </button>
-        <button
-          onClicked={() =>
-            execAsync([
-              "sh",
-              "-c",
-              "XDG_CURRENT_DESKTOP=GNOME gnome-control-center",
-            ])
-          }
-        >
-          <label label={bind(network.wifi, "ssid")} />
-        </button>
-      </box>
+      <WiFiBox />
 
       {/* Bluetooth Toggle */}
       <box cssClasses={["toggle"]}>
